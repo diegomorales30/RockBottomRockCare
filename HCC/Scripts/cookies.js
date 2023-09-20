c47f2efad2de4af26a5d983f2741c6ea494177be
diegomorales30 <diegovmo96@gmail.com>
2023-09-20 15:27:22 -0600

    Creates Infinit loop This loop goes until a unique key is created and put in the server.

--- HCC/Scripts/cookies.js
+++ HCC/Scripts/cookies.js
@@ -4,15 +4,36 @@ function createCookie(cookieCheck){
 
     let i = 0;
     let strLength = 8;
-    while(i <= strLength){
-        let min = 65;
-        let max = 90;
-        let ranNum = Math.random() * (max - min) + min;
-        let ranNumRound = Math.floor(ranNum);
-        let chr = String.fromCharCode(ranNumRound);
-        coookieStr = coookieStr + chr;
-        i = i + 1;
-}
+
+    while(true){
+
+        while(i <= strLength){
+            let min = 65;
+            let max = 90;
+            let ranNum = Math.random() * (max - min) + min;
+            let ranNumRound = Math.floor(ranNum);
+            let chr = String.fromCharCode(ranNumRound);
+            coookieStr = cookieCheck + "rock=" +  chr;
+            i = i + 1;
+        }
+    
+        let arrStr = coookieStr.split("=");
+    
+        let checkCook = arrStr[1];
+
+        if(isValidCookie(checkCook)){
+            //Post Function
+            break;
+        }
+
+
+
+
+    }
+
+    
+
+
 
     
 }
@@ -20,12 +41,13 @@ function createCookie(cookieCheck){
 
 
 window.onload = (event) => {
-    cookieCheck = document.cookie
+    let cookieCheck = ""
 
-    if(cookieCheck == ""){
+    if(document.cookie){
         console.log("No Cookie")
         createCookie(cookieCheck)
     }
+    // the search method will go in this part of the code
     console.log("Welcome back")
     return
 };
