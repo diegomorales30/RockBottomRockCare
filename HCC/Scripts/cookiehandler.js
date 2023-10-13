9ce4df99b6282a91f64955f55203a7c4bd0a3af8
Cassius-CS <cashs0426@gmail.com>
2023-10-13 15:10:05 -0600

    Added comments to request method

--- HCC/Scripts/cookiehandler.js
+++ HCC/Scripts/cookiehandler.js
@@ -1,14 +1,20 @@
 export async function addCookie(cookieName) {
+    // Create header to add to request
     var requestHeaders = new Headers()
+    // Specify what type of request we are sending
     requestHeaders.append("Type", "AddCookie");
+    // Specify other options like POST, cors, and request body
     var requestOptions = {
         method: "POST",
         headers: requestHeaders,
         mode: "cors",
         body: cookieName
     };
+    // Create the request to our website and pass in our options
     var request = new Request("https://rockbottomrockcare.com/cookie", requestOptions);
+    // Create response variable
     var response = await fetch(request);
+    // Handle the response with a promise
     await response.text().then(text => {
         console.log("Added cookie: ", text);
     });
